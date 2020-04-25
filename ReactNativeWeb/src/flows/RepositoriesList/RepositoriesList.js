80a980032baebcd781702d2b33f548970d879711
Federico Ojeda <federico@austinsoftware.com>
2020-04-25 16:21:32 -0300

    added a back button

--- ReactNativeWeb/src/flows/RepositoriesList/RepositoriesList.js
+++ ReactNativeWeb/src/flows/RepositoriesList/RepositoriesList.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+  Platform,
+} from 'react-native';
+import {useHistory} from 'react-router';
+
+const styles = StyleSheet.create({
+  backButton: {margin: 10, fontSize: 18, fontWeight: '200'},
+  mainText: {marginTop: 40, textAlign: 'center', fontSize: 32},
+});
 
 function RepositoriesList() {
+  const history = useHistory();
   return (
     <SafeAreaView>
       <View>
-        <Text>Repositories!</Text>
+        <TouchableOpacity
+          onPress={() => {
+            history.goBack();
+          }}>
+          <Text style={styles.backButton}>{'<- Back'}</Text>
+        </TouchableOpacity>
+        <Text style={styles.mainText}>Repositories!</Text>
+        <Text style={styles.mainText}>{Platform.OS}</Text>
       </View>
     </SafeAreaView>
   );
